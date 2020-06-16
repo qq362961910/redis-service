e89932342fcf5a0b42c82237ecd650c92c6afd59
yangjian <yangjian@ishansong.com>
2020-06-16 21:00:40 +0800

    补充redis-service-demo模块

---
+++ redis-service-demo/src/main/java/cn/t/redisservice/client/demo/ShardedRedisServerClusterTest.java
@@ -0,0 +1,11 @@
+package cn.t.redisservice.client.demo;
+
+/**
+ * @author yj
+ * @since 2020-06-16 21:00
+ **/
+public class ShardedRedisServerClusterTest {
+    public static void main(String[] args) {
+
+    }
+}
